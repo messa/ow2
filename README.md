5c4886ba9ca95b86818cf1ef422921c12c46cab1
Petr Messner <petr.messner@gmail.com>
2019-03-10 17:17:42 +0100

    hub: restructure stream model

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This is a temporary repository for prototyping new version of the monitoring system.
+
+This is under development. For example no data migrations may be provided.
--- hub/overwatch_hub/model/streams.py
+++ hub/overwatch_hub/model/streams.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 from logging import getLogger
 
 from ..util import random_str
@@ -7,34 +7,28 @@ from ..util import random_str
 logger = getLogger(__name__)
 
 
-class StreamLabels:
+class Streams:
 
     def __init__(self, db):
-        self._c_labels = db['streams.labels']
+        self._c_labels = db['streams']
 
     async def create_mandatory_indexes(self):
         await self._c_labels.create_index('label_str', unique=True)
 
-    async def resolve_label_id(self, label):
+    async def resolve_id_by_label(self, label):
         assert isinstance(label, dict)
         label_str = get_label_str(label)
-        now = datetime.utcnow()
         doc = await self._c_labels.find_one({'label_str': label_str})
         if doc:
-            if now - doc['last_used'] > timedelta(hours=1):
-                await self._c_labels.update_one(
-                    {'_id': doc['_id']},
-                    {'$set': {'last_used': now}})
             return doc['_id']
         doc = {
             '_id': random_str(8),
             'label_str': label_str,
             'label': label,
-            'created': {'date': now},
-            'last_used': now,
+            'created': {'date': datetime.utcnow()},
         }
         await self._c_labels.insert_one(doc)
-        logger.debug('Inserted new stream label %r %r', doc['_id'], doc['label'])
+        logger.debug('Inserted new stream id: %r label: %r', doc['_id'], doc['label'])
         return doc['_id']
 
 
