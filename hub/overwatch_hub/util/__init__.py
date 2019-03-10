3ecf4476ed65dea239e834b3c0f257a3b828ea33
Petr Messner <petr.messner@gmail.com>
2019-03-10 16:44:47 +0100

    hub: add stream_labels and stream_snapshots model

--- hub/overwatch_hub/util/__init__.py
+++ hub/overwatch_hub/util/__init__.py
@@ -1,2 +1,2 @@
-from .misc import smart_repr
+from .misc import parse_datetime, random_str, smart_repr, to_compact_json
 from .mongodb import get_mongo_db_name
--- hub/overwatch_hub/util/misc.py
+++ hub/overwatch_hub/util/misc.py
@@ -1,4 +1,8 @@
+from datetime import datetime
+import pytz
 import reprlib
+from secrets import token_urlsafe
+from simplejson import dumps as json_dumps
 
 
 _repr_obj = reprlib.Repr()
@@ -6,3 +10,29 @@ _repr_obj.maxstr = 60
 _repr_obj.maxother = 60
 
 smart_repr = _repr_obj.repr
+
+
+def parse_datetime(dt_str):
+    if not isinstance(dt_str, str):
+        raise Exception(f'Expected str: {dt_str!r}')
+    try:
+        if dt_str.endswith('Z'):
+            dt_str = dt_str[:-1] + '+00:00'
+        dt = datetime.fromisoformat(dt_str)
+        dt = pytz.utc.normalize(dt) if dt.tzinfo else pytz.utc.localize(dt)
+        return dt
+    except Exception as e:
+        raise Exception(f'Failed to parse datetime {dt_str!r}: {e!r}')
+
+
+def random_str(length):
+    while True:
+        x = token_urlsafe(length)
+        x = x[:length]
+        if not x.isalnum():
+            continue
+        return x
+
+
+def to_compact_json(obj):
+    return json_dumps(obj, separators=(',', ':'))
