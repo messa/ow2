d87b3ba3b3ea6c1a8a21050b4b4475a6aa0f4315
Petr Messner <petr.messner@gmail.com>
2019-03-29 14:45:09 +0100

    web: basic CSS

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import Container from '../components/ui/Container'
 
 export default () => (
   <Layout>
-    <h1>Overwatch monitoring</h1>
+    <Container>
+      <h1>Overwatch monitoring</h1>
+      <p>Light as a feather at ~400 lines & built with mobile in mind.</p>
+    </Container>
   </Layout>
 )
