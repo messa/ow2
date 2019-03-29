1b5f79bff695a0ad3b57252e11ae2837001fbcea
Petr Messner <petr.messner@gmail.com>
2019-03-29 14:09:59 +0100

    web: update dependencies

--- web/components/Layout.js
+++ web/components/Layout.js
@@ -1,16 +1,14 @@
 import Head from 'next/head'
-import { Container } from 'semantic-ui-react'
 
 export default ({ children }) => (
   <div className='Layout'>
     <Head>
-      <title>Pyladies Courseware</title>
+      <title>Overwatch monitoring</title>
       <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, shrink-to-fit=no" />
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
     </Head>
-    <Container>
+    <div>
       <br/>
       {children}
-    </Container>
+    </div>
   </div>
 )
