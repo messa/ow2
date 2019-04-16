4d99a4d26f798eb140bf31c2f6640486ac6a9d47
Petr Messner <petr.messner@gmail.com>
2019-04-16 11:55:02 +0200

    web: WIP GraphQL based on next.js example with-relay-modern

--- web/pages/streams.js
+++ web/pages/streams.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import { graphql } from 'react-relay'
 import Layout from '../components/Layout'
 import Container from '../components/ui/Container'
+import withData from '../lib/withData'
 
-export default () => (
-  <Layout activeItem='streams'>
-    <Container>
-    </Container>
-  </Layout>
-)
+class StreamsPage extends React.Component {
+
+  render() {
+    return (
+      <Layout activeItem='streams'>
+        <Container>
+        </Container>
+      </Layout>
+    )
+  }
+
+}
+
+export default withData(StreamsPage, {
+  query: graphql`
+    query streamsQuery {
+      streams {
+        edges {
+          node {
+            streamId
+          }
+        }
+      }
+    }
+  `
+})
