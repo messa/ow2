b30bc87e49f887c50bdf95894c0244bb8a9f413d
Petr Messner <petr.messner@gmail.com>
2019-04-16 10:38:26 +0200

    hub: switch to graphene

--- hub/overwatch_hub/graphql/schema.py
+++ hub/overwatch_hub/graphql/schema.py
@@ -1,61 +1,111 @@
+import asyncio
+from graphene import ObjectType, Int, String, Schema
+from graphene.relay import Node, Connection, ConnectionField
+from graphene.types.json import JSONString
 from logging import getLogger
-from graphql import (
-    graphql,
-    GraphQLSchema,
-    GraphQLInterfaceType,
-    GraphQLObjectType,
-    GraphQLField,
-    GraphQLInputObjectField,
-    GraphQLArgument,
-    GraphQLNonNull,
-    GraphQLString,
-    GraphQLInt,
-    GraphQLList,
-    GraphQLID,
-    GraphQLBoolean,
-)
 
-from .relay_helpers import connection_args, connection_from_list, relay_connection_type, mutation
+from .helpers import Obj, json_dumps
 
 
 logger = getLogger(__name__)
 
 
-async def node_resolver():
-    raise NotImplementedError()
+'''
+class Record (ObjectType):
+
+    class Meta:
+        interfaces = (Node, )
+
+    @classmethod
+    def get_node(cls, info, id):
+        raise Exception('NIY')
+
+    record_id = String(name='recordId')
+    pack_id = String(name='packId')
+    record = JSONString()
+
+    #pack = lambda: Pack()
+
+    def resolve_record_id(record, info):
+        return str(record.id)
 
 
-def resolve_node_type(obj, info):
-    # See also: https://stackoverflow.com/q/34726666/196206
-    # if obj.node_type == 'Post':
-    #     return Post
-    # if obj.node_type == 'Category':
-    #     return Category
-    raise Exception(f'Unknown node type: {obj!r}')
+class RecordConnection (Connection):
+
+    class Meta:
+        node = Record
+'''
+
+'''
+class Pack (ObjectType):
+
+    class Meta:
+        interfaces = (Node, )
+
+    @classmethod
+    def get_node(cls, info, id):
+        raise Exception('NIY')
+
+    label_json = String(name='labelJSON')
+    pack_id = String(name='packId')
+    records = ConnectionField(RecordConnection)
+    fresh_record_count = Int()
+
+    def resolve_pack_id(root, info):
+        return root.id
+
+    async def resolve_records(pack, info):
+        model = info.context['request'].app['model']
+        records = await model.fresh_records.list_by_pack_id(pack.id)
+        return records
+
+    async def resolve_fresh_record_count(pack, info):
+        model = info.context['request'].app['model']
+        return await model.fresh_records.count_by_pack_id(pack.id)
 
 
-NodeInterface = GraphQLInterfaceType(
-    name='Node',
-    fields={
-        'id': GraphQLField(type=GraphQLNonNull(GraphQLID)),
-    },
-    resolve_type=resolve_node_type)
+class PackConnection (Connection):
+
+    class Meta:
+        node = Pack
+'''
 
 
-Schema = GraphQLSchema(
-    query=GraphQLObjectType(
-        name='Query',
-        fields={
-            'node': GraphQLField(
-                type=NodeInterface,
-                args={
-                    'id': GraphQLArgument(GraphQLNonNull(GraphQLID)),
-                },
-                resolver=node_resolver),
-            # 'categories': GraphQLField(
-            #     type=CategoryConnection,
-            #     args=connection_args,
-            #     resolver=categories_resolver),
-        }
-    ),
-)
+class Stream (ObjectType):
+
+    class Meta:
+        interfaces = (Node, )
+
+    @classmethod
+    def get_node(cls, info, id):
+        raise Exception('NIY')
+
+    label_json = String(name='labelJSON')
+    stream_id = String(name='streamId')
+
+    def resolve_stream_id(stream, info):
+        return stream.id
+
+    def resolve_label_json(stream, info):
+        return json_dumps(stream.label)
+
+
+class StreamConnection (Connection):
+
+    class Meta:
+        node = Stream
+
+
+class Query (ObjectType):
+
+    node = Node.Field()
+
+    streams = ConnectionField(StreamConnection)
+
+    async def resolve_streams(self, info):
+        model = info.context['request'].app['model']
+        streams = await model.streams.list_all()
+        return streams
+
+
+graphql_schema = Schema(query=Query)
