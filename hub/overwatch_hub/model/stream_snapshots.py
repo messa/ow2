1982158f8cb0bc8ae1681acf0c4e339c5417a9b4
Petr Messner <petr.messner@gmail.com>
2019-04-18 23:09:25 +0200

    hub: add endpoint /dump-snapshots

--- hub/overwatch_hub/model/stream_snapshots.py
+++ hub/overwatch_hub/model/stream_snapshots.py
@@ -1,3 +1,4 @@
+from asyncio import shield
 from bson import ObjectId
 from datetime import datetime
 from logging import getLogger
@@ -6,6 +7,7 @@ from pymongo import DESCENDING as DESC
 from pytz import utc
 
 from ..util import to_compact_json
+from .helpers import to_objectid
 
 
 logger = getLogger(__name__)
@@ -15,6 +17,7 @@ class StreamSnapshots:
 
     def __init__(self, db):
         self._c_snapshots = db['streams.snapshots']
+        self._c_states = db['streams.snapshots.states']
 
     async def create_optional_indexes(self):
         await self._c_snapshots.create_index([
@@ -24,33 +27,65 @@ class StreamSnapshots:
 
     async def insert(self, date, stream_id, state):
         assert isinstance(date, datetime)
+        assert isinstance(stream_id, str)
         assert isinstance(state, dict)
-        doc = {
+        doc_snapshot = {
             '_id': ObjectId(),
             'stream_id': stream_id,
             'date': date,
+        }
+        doc_state = {
+            '_id': doc_snapshot['_id'],
             'state_json': to_compact_json(state),
         }
-        await self._c_snapshots.insert_one(doc)
-        logger.info('Inserted %s %s', self._c_snapshots.name, doc['_id'])
+        await self._c_states.insert_one(doc_state)
+        await self._c_snapshots.insert_one(doc_snapshot)
+        logger.info('Inserted %s %s', self._c_snapshots.name, doc_snapshot['_id'])
+        return await self._obj(doc_snapshot, doc_state)
 
     async def get_latest(self, stream_id):
         assert isinstance(stream_id, str)
         doc = await self._c_snapshots.find_one(
             {'stream_id': stream_id},
             sort=[('date', DESC)])
-        return self._obj(doc)
+        return await self._obj(doc)
 
-    def _obj(self, doc):
-        return StreamSnapshot(doc)
+    async def _obj(self, doc_snapshot, doc_state=None):
+        if not doc_state and not doc_snapshot.get('state_json'):
+            doc_state = await self._c_states.find_one({'_id': doc_snapshot['_id']})
+        return StreamSnapshot(doc_snapshot, doc_state)
+
+    async def dump(self, stream_id=None, after_snapshot_id=None):
+        q = {}
+        if stream_id:
+            assert isinstance(stream_id, str)
+            q['stream_id'] = stream_id
+        if after_snapshot_id:
+            q['_id'] = {'$gt': to_objectid(after_snapshot_id)}
+        snapshot_docs = await shield(self._c_snapshots.find(q, sort=[('_id', ASC)]).to_list(length=500))
+        state_q = {'_id': {'$in': [d['_id'] for d in snapshot_docs]}}
+        state_docs = await shield(self._c_states.find(state_q).to_list(length=None))
+        state_docs = {d['_id']: d for d in state_docs}
+        out = []
+        for snapshot_doc in snapshot_docs:
+            doc_state = state_docs.get(snapshot_doc['_id'])
+            if not doc_state and not snapshot_doc['state_json']:
+                logger.info('%s id %s not found', self._c_states.name, snapshot_doc['_id'])
+                continue
+            out.append(StreamSnapshot(
+                doc_snapshot=snapshot_doc,
+                doc_state=doc_state,
+            ))
+        return out
 
 
 class StreamSnapshot:
 
     __slots__ = ('id', 'date', 'state_json', 'stream_id')
 
-    def __init__(self, doc):
-        self.id = doc['_id']
-        self.date = utc.localize(doc['date'])
-        self.state_json = doc['state_json']
-        self.stream_id = doc['stream_id']
+    def __init__(self, doc_snapshot, doc_state):
+        assert not doc_state or doc_snapshot['_id'] == doc_state['_id']
+        self.id = doc_snapshot['_id']
+        self.date = utc.localize(doc_snapshot['date'])
+        self.stream_id = doc_snapshot['stream_id']
+        self.state_json = doc_snapshot.get('state_json') or doc_state['state_json']
