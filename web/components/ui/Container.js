21d7c17bcd686fef158088cb7485fecc666c1c0e
Petr Messner <petr.messner@gmail.com>
2019-04-18 20:54:46 +0200

    web: CSS container

--- web/components/ui/Container.js
+++ web/components/ui/Container.js
@@ -5,8 +5,22 @@ export default ({ children }) => (
     {children}
     <style jsx>{`
       .owContainer {
-        margin-left: 16px;
-        margin-right: 16px;
+        margin-left: 15px;
+        margin-right: 15px;
+      }
+      @media (min-width: 550px) {
+        .owContainer {
+          margin-left: 20px;
+          margin-right: 20px;
+        }
+      }
+      @media (min-width: 1000px) {
+        .owContainer {
+          width: 100%;
+          max-width: 960px;
+          margin: 0 auto;
+          padding: 0 20px;
+        }
       }
     `}</style>
   </div>
