3069dd7f5325204d070489b6d56273ce2dce9354
Petr Messner <petr.messner@gmail.com>
2019-04-19 13:18:53 +0200

    web: CSS container jsx -> static

--- web/components/ui/Container.js
+++ web/components/ui/Container.js
@@ -3,25 +3,5 @@ import React from 'react'
 export default ({ children }) => (
   <div className='owContainer'>
     {children}
-    <style jsx>{`
-      .owContainer {
-        margin-left: 15px;
-        margin-right: 15px;
-      }
-      @media (min-width: 550px) {
-        .owContainer {
-          margin-left: 20px;
-          margin-right: 20px;
-        }
-      }
-      @media (min-width: 1000px) {
-        .owContainer {
-          width: 100%;
-          max-width: 960px;
-          margin: 0 auto;
-          padding: 0 20px;
-        }
-      }
-    `}</style>
   </div>
 )
