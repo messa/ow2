89a643a8d325a05ef520fc1ce35a255cf6dfdb8d
Petr Messner <petr.messner@gmail.com>
2019-04-21 15:47:54 +0200

    WIP snapshot item search

--- hub/overwatch_hub/model/streams.py
+++ hub/overwatch_hub/model/streams.py
@@ -1,3 +1,4 @@
+from bson import ObjectId
 from datetime import datetime
 from logging import getLogger
 
@@ -10,10 +11,10 @@ logger = getLogger(__name__)
 class Streams:
 
     def __init__(self, db):
-        self._c_labels = db['streams']
+        self._c_streams = db['streams']
 
     async def create_mandatory_indexes(self):
-        await self._c_labels.create_index('label_str', unique=True)
+        await self._c_streams.create_index('label_str', unique=True)
 
     async def resolve_id_by_label(self, label):
         stream = await self.resolve_by_label(label)
@@ -27,7 +28,7 @@ class Streams:
             if try_count > 3:
                 raise Exception(f'Too many tries: resolve_by_label({label!r})')
             try_count += 1
-            doc = await self._c_labels.find_one({'label_str': label_str})
+            doc = await self._c_streams.find_one({'label_str': label_str})
             if doc:
                 return self._obj(doc)
             doc = {
@@ -35,34 +36,54 @@ class Streams:
                 'label_str': label_str,
                 'label': label,
                 'created': {'date': datetime.utcnow()},
+                'last_snapshot_id': None,
             }
             try:
-                await self._c_labels.insert_one(doc)
+                await self._c_streams.insert_one(doc)
             except Exception as e:
-                logger.debug('Failed to insert %s %r: %r; trying again', self._c_labels.name, doc, e)
+                logger.debug('Failed to insert %s %r: %r; trying again', self._c_streams.name, doc, e)
                 continue
             logger.debug('Inserted new stream id: %r label: %r', doc['_id'], doc['label'])
             return self._obj(doc)
+
+    async def update_last_snapshot_id(self, stream_id, snapshot_id):
+        assert isinstance(stream_id, str)
+        assert isinstance(snapshot_id, ObjectId)
+        await self._c_streams.update_one(
+            {
+                '_id': stream_id,
+                '$or': [
+                    {'last_snapshot_id': None},
+                    {'last_snapshot_id': {'$lt': snapshot_id}},
+                ],
+            }, {
+                '$set': {
+                    'last_snapshot_id': snapshot_id,
+                }
+            })
 
     def _obj(self, doc):
         return Stream(doc)
 
     async def get_by_id(self, stream_id):
         assert isinstance(stream_id, str)
-        doc = await self._c_labels.find_one({'_id': stream_id})
+        doc = await self._c_streams.find_one({'_id': stream_id})
         return self._obj(doc)
 
     async def list_all(self):
-        docs = await self._c_labels.find({}).to_list(10**4)
+        docs = await self._c_streams.find({}).to_list(10**4)
         return [self._obj(d) for d in docs]
 
 
 class Stream:
 
+    __slots__ = ('id', 'label', 'created_date', 'last_snapshot_id')
+
     def __init__(self, doc):
         self.id = doc['_id']
         self.label = doc['label']
         self.created_date = doc['created']['date']
+        self.last_snapshot_id = doc['last_snapshot_id']
 
 
 def get_label_str(label):
