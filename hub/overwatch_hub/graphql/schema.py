c0f439c32a20c7f5641869aa3e29b622a9304dcf
Petr Messner <petr.messner@gmail.com>
2019-04-21 12:20:59 +0200

    hub: clean up old code
    Originally just copy-pasted from different project as a gql schema stub.

--- hub/overwatch_hub/graphql/schema.py
+++ hub/overwatch_hub/graphql/schema.py
@@ -11,67 +11,6 @@ from .helpers import Obj, json_dumps
 logger = getLogger(__name__)
 
 
-'''
-class Record (ObjectType):
-
-    class Meta:
-        interfaces = (Node, )
-
-    @classmethod
-    def get_node(cls, info, id):
-        raise Exception('NIY')
-
-    record_id = String(name='recordId')
-    pack_id = String(name='packId')
-    record = JSONString()
-
-    #pack = lambda: Pack()
-
-    def resolve_record_id(record, info):
-        return str(record.id)
-
-
-class RecordConnection (Connection):
-
-    class Meta:
-        node = Record
-'''
-
-'''
-class Pack (ObjectType):
-
-    class Meta:
-        interfaces = (Node, )
-
-    @classmethod
-    def get_node(cls, info, id):
-        raise Exception('NIY')
-
-    label_json = String(name='labelJSON')
-    pack_id = String(name='packId')
-    records = ConnectionField(RecordConnection)
-    fresh_record_count = Int()
-
-    def resolve_pack_id(root, info):
-        return root.id
-
-    async def resolve_records(pack, info):
-        model = info.context['request'].app['model']
-        records = await model.fresh_records.list_by_pack_id(pack.id)
-        return records
-
-    async def resolve_fresh_record_count(pack, info):
-        model = info.context['request'].app['model']
-        return await model.fresh_records.count_by_pack_id(pack.id)
-
-
-class PackConnection (Connection):
-
-    class Meta:
-        node = Pack
-'''
-
-
 class StreamSnapshotMetadata (ObjectType):
 
     class Meta:
