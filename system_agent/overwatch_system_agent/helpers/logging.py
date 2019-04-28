e07b9b3368cc8bf2c12c896fd56459b41298eb53
Petr Messner <petr.messner@gmail.com>
2019-04-28 17:27:48 +0200

    system agent: minor logging setup refactoring

--- system_agent/overwatch_system_agent/helpers/logging.py
+++ system_agent/overwatch_system_agent/helpers/logging.py
@@ -5,21 +5,22 @@ log_format = '%(asctime)s %(name)-30s %(levelname)5s: %(message)s'
 
 
 def setup_logging(verbosity=0):
-    from logging import DEBUG, INFO, ERROR, Formatter, StreamHandler, getLogger
-
-    if not verbosity:
-        console_level = ERROR
-    elif verbosity == 1:
-        console_level = INFO
-    else:
-        console_level = DEBUG
-
+    from logging import DEBUG, Formatter, StreamHandler, getLogger
     getLogger().setLevel(DEBUG)
-
     h = StreamHandler()
-    h.setLevel(console_level)
+    h.setLevel(get_level_by_verbosity(verbosity))
     h.setFormatter(Formatter(log_format))
     getLogger().addHandler(h)
+
+
+def get_level_by_verbosity(verbosity):
+    from logging import DEBUG, ERROR, INFO
+    if not verbosity:
+        return ERROR
+    elif verbosity == 1:
+        return INFO
+    else:
+        return DEBUG
 
 
 def setup_log_file(log_file_path):
