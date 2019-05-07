44cf8586457ce73f74f4a90735672cdc4751e509
Petr Messner <petr.messner@gmail.com>
2019-05-07 14:29:09 +0200

    web: temporarily add bunch of font <link>s
    So I can play with it on a deployed production instance.
    Will be removed again soon.

--- web/components/Layout.js
+++ web/components/Layout.js
@@ -9,6 +9,14 @@ export default ({ children, activeItem }) => (
       <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, shrink-to-fit=no" />
       <link href='//fonts.googleapis.com/css?family=Raleway:400,300,600' rel='stylesheet' type='text/css' />
       <link href="//fonts.googleapis.com/css?family=Inconsolata:400,700&amp;subset=latin-ext" rel="stylesheet" />
+      <link href="//fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900&amp;subset=latin-ext" rel="stylesheet" />
+      <link href="//fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700&amp;subset=latin-ext" rel="stylesheet" />
+      <link href="//fonts.googleapis.com/css?family=Roboto+Slab:300,400,700&amp;subset=latin-ext" rel="stylesheet" />
+      <link href="//fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800&amp;subset=latin-ext" rel="stylesheet" />
+      <link href="//fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700&amp;subset=latin-ext" rel="stylesheet" />
+      <link href="//fonts.googleapis.com/css?family=Lato:300,400,700,900&amp;subset=latin-ext" rel="stylesheet" />
+      <link href="//fonts.googleapis.com/css?family=PT+Sans:400,700&amp;subset=latin-ext" rel="stylesheet" />
+      <link href="//fonts.googleapis.com/css?family=PT+Sans+Narrow:400,700&amp;subset=latin-ext" rel="stylesheet" />
       <link rel='stylesheet' type='text/css' href='/static/css/normalize.css' />
       <link rel='stylesheet' type='text/css' href='/static/css/skeleton.css' />
       <link rel='stylesheet' type='text/css' href='/static/css/overwatch.css' />
